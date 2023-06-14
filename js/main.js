60f2f7a32f1bf1d33994d1b837ca7079b861431f
marcocampisi <marco.campisi@outlook.it>
2023-06-14 17:03:52 +0200

    Added graphical interface (completed bonus)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
+// const number = document.createElement('div');
+// number.classList.add('col', 'number');
+// const fizz = document.createElement('div');
+// fizz.classList.add('col', 'fizz');
+// const buzz = document.createElement('div');
+// buzz.classList.add('col', 'buzz');
+// const fizzbuzz = document.createElement('div');
+// fizzbuzz.classList.add('col', 'fizzbuzz');
+const fizzbuzzGrid = document.getElementById('fizzbuzzGrid');
+
 for (let i = 1; i <= 100; i++){
     if (i % 15 == 0 ){
-        console.log('FizzBuzz');
+        const fizzbuzz = document.createElement('div');
+        fizzbuzz.classList.add('col-2', 'm-3', 'square', 'fizzbuzz');
+        fizzbuzz.innerHTML = 'Fizzbuzz';
+        fizzbuzzGrid.append(fizzbuzz);
     } else if (i % 3 == 0) {
-        console.log('Fizz');
+        const fizz = document.createElement('div');
+        fizz.classList.add('col-2', 'm-3', 'square', 'fizz');
+        fizz.innerHTML = 'Fizz';
+        fizzbuzzGrid.append(fizz);
     } else if (i % 5 == 0) {
-        console.log('Buzz');
+        const buzz = document.createElement('div');
+        buzz.classList.add('col-2', 'm-3', 'square', 'buzz');
+        buzz.innerHTML = 'Buzz';
+        fizzbuzzGrid.append(buzz);
     } else {
-        console.log(i);
+        const number = document.createElement('div');
+        number.classList.add('col-2', 'm-3', 'square', 'number');
+        number.innerHTML = i;
+        fizzbuzzGrid.append(number);
     }
 }
